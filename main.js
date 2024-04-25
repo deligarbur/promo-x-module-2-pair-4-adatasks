1bc987f69246798cc755f1e4d81dca7d221f5beb
Diana <dianaga93@hotmail.com>
2024-04-25 12:25:17 +0200

    event click

--- main.js
+++ main.js
@@ -3,23 +3,38 @@
 const taskList = document.querySelector('.js-task-list');
 
 const tasks = [
-    { name: 'Recoger setas en el campo', completed: true },
-    { name: 'Comprar pilas', completed: true },
-    { name: 'Poner una lavadora de blancos', completed: true },
+    { name: 'Recoger setas en el campo', completed: true, id:'1' },
+    { name: 'Comprar pilas', completed: true, id:'2' },
+    { name: 'Poner una lavadora de blancos', completed: true, id:'3' },
     {
       name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-      completed: false,
+      completed: false, id:'4',
     }
 ];
 
 console.log(tasks);
 
+const handleClick = (ev) =>{
+  const checkClick = ev.currentTarget;
+  
+  const index = tasks.findIndex((item) => item.id === checkClick )
+  tasks[index].completed =  !tasks[index].completed;
+  console.log(index);
+}
+
+const listener = () =>{
+  const checkbox = document.querySelectorAll('.js-input');
+  for (const check of checkbox) {
+    check.addEventListener('click',handleClick);    
+  }
+
+}
 
 const renderTasks = (array) => {
   taskList.innerHTML = '';
   array.forEach((item) => {
     if(item.completed === true){
-      taskList.innerHTML += `<li class="tachado">
+      taskList.innerHTML += `<li class="tachado js-li">
     <input checked type="checkbox" class="js-input">
     ${item.name}
     </li>`
@@ -29,7 +44,7 @@ const renderTasks = (array) => {
     ${item.name}
     </li>`
     }
-    
+    listener ();
   });
 };
 
@@ -37,17 +52,6 @@ renderTasks(tasks);
 
 // const inputCheck = document.querySelectorAll('.js-input');
 
-
-handleClick =(ev) =>{
-  const checked = ev.target;
-  for (const itemcheked of tasks) {
-    
-  }
-
-}
-
-taskList.addEventListener('click', handleClick);
-  
 
  /* Duda donde meter el if ( si es la manejadora o en el handle) y si necesitamos un bucle para hacer el if dentro.*/
  // Una vez soucionado tendremos que hacer un map para modificar la clase y un classlist.add para añadir la clase de tachado
